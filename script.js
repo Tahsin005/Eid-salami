9c074b67792ab615382ec0834921fd8e2542fd30
tahsin005 <tahsin.ferdous3546@gmail.com>
2024-04-09 17:16:55 +0600

    Eid salami nibi?

---
+++ script.js
@@ -0,0 +1,5 @@
+const openGiftButton = document.getElementById('open-gift');
+
+openGiftButton.addEventListener('click', () => {
+    window.location.href = "surprise.html";
+});
---
+++ yesno.js
@@ -0,0 +1,10 @@
+const yesButton = document.getElementById('yes-button');
+const noButton = document.getElementById('no-button');
+
+yesButton.addEventListener('click', () => {
+  window.location.href = "prank.html";
+});
+
+noButton.addEventListener('click', () => {
+  window.location.href = "prank.html";
+});
